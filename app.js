ed1cf709ffb0d1f49052b6c5b74df2cd97d0b26f
chinmayakain <chinmaynaik.009@gmail.com>
2022-04-23 12:49:46 +0530

    cors issue

--- app.js
+++ app.js
@@ -16,8 +16,13 @@ var port = process.env.PORT || 3000;
 /** this project needs a db !! **/
 // mongoose.connect(process.env.DB_URI);
 
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
 
-app.use(cors());
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
